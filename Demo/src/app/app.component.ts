adfc6a9741661fdade62c779dd4e0047c74dbbd2
SurajSalunkhe7 <salunkhesuraj7768@gmail.com>
2023-07-19 20:29:53 +0530

    Demo

---
+++ Demo/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({    //Decorater
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  title = 'Demo';   // Characteristic
+  name = 'Suraj Hanmant Salunkhe';  // Characteristics
+  Age = 21;     // Characteristics
+}
